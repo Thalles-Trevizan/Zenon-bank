d40cd3b67cc315ff9567383e644121c7869b9eaf
Thalles-Trevizan <55063360+Thalles-Trevizan@users.noreply.github.com>
2026-06-24 12:00:45 -0300

    Adicionando tratamento de erros (#3)

--- src/main/java/br/com/zenon/Main.java
+++ src/main/java/br/com/zenon/Main.java
@@ -10,17 +10,17 @@ import java.util.List;
 
 public class Main {
 
-    void main(){
+    void main() {
         var t1 = new Transaction(
                 1,
                 TransactionType.PAYMENT,
                 new BigDecimal("9839.64"),
                 new TransactionCustomer("C1231006815",
-                    new BigDecimal("170136.00"),
-                    new BigDecimal("160296.36")),
+                        new BigDecimal("170136.00"),
+                        new BigDecimal("160296.36")),
                 new TransactionCustomer("M1979787155",
-                    new BigDecimal("0.00"),
-                    new BigDecimal("0.00")),
+                        new BigDecimal("0.00"),
+                        new BigDecimal("0.00")),
                 true,
                 false
         );
@@ -30,11 +30,11 @@ public class Main {
                 TransactionType.CASH_OUT,
                 new BigDecimal("850002.52"),
                 new TransactionCustomer("C1280323807",
-                    new BigDecimal("850002.52"),
-                    new BigDecimal("0.00")),
+                        new BigDecimal("850002.52"),
+                        new BigDecimal("0.00")),
                 new TransactionCustomer("C873221189",
-                    new BigDecimal("6510099.11"),
-                    new BigDecimal("7360101.63")),
+                        new BigDecimal("6510099.11"),
+                        new BigDecimal("7360101.63")),
                 true,
                 false
         );
@@ -46,6 +46,16 @@ public class Main {
 
         TransactionIngestor ingestor = new TransactionIngestor();
         List<Transaction> transactions = ingestor.read("data/PS_20174392719_1491204439457_log.csv");
-        transactions.stream().limit(10).forEach(System.out::println);
+        transactions.stream()
+                .limit(10)
+                .forEach(System.out::println);
+
+        System.out.println("===============================================");
+
+        List<Transaction> transactionsBadData = ingestor.read("data/paysim_with_bad_data.csv");
+
+        System.out.println(transactionsBadData.size());
+        transactionsBadData
+                .forEach(System.out::println);
     }
 }
